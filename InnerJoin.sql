2f5e900cd49c152d9a73cf09846def8a7a53e4b6
0Jesus-Rojas <127401558+0Jesus-Rojas@users.noreply.github.com>
2026-04-22 11:40:57 -0500

    Add files via upload

--- InnerJoin.sql
+++ InnerJoin.sql
@@ -40,3 +40,68 @@ inner join actividades on id_actividades = id_grupo;
 select fecha_asistencia, estado
 from asistencia
 inner join estadoasistencia on id_estado_asistencia = FK_id_estado_asistencia;
+
+/* ==== 6. Mostrar niño y docente que le da clase ====*/
+
+select nombre_nino as Niño, nombre_docente as Docente
+from Ninos
+inner join Grupos on FK_id_grupo = id_grupo
+inner join docentes on FK_id_docente = id_docentes;
+
+/* ==== 7. Mostrar niño, grupo y actividad ====*/
+
+select nombre_nino as Niño, nombre_grupo as Grupo, descripcion_actividad as Actividad
+from ninos
+inner join Grupos on id_ninos = id_grupo 
+inner join actividades on id_grupo = id_actividades;
+
+/* ==== 8. Mostrar niño acudiente y parentesco ====*/
+
+select nombre_nino as Niño, nombre_acudiente as Acudiente, parentesco
+from ninos
+inner join acudiente on id_acudiente = id_ninos;
+
+/*==== 9. Mostrar niño la fecha de la asistencia y el estado ====*/
+
+select id_asistencia as Asistencia, nombre_nino as Nombre_Niño, fecha_asistencia as Fecha_Asistencia, estado as Estado 
+From ninos
+left join asistencia on id_ninos = FK_id_ninos
+join estadoasistencia on id_estado_asistencia = FK_id_estado_asistencia;
+
+/* ==== 10. Mostrar niño y registro medico ====*/
+
+select nombre_nino as Niño, observaciones as Registro_medicos, Alergias
+from ninos
+inner join registromedico on id_ninos = FK_id_ninos;
+
+/* ==== 11.Mostrar niño, docente, acudiente y grupo ====*/
+
+select nombre_nino as Niño, nombre_acudiente as Acudiente, nombre_grupo as Grupo, nombre_docente as Docente
+from ninos
+join grupos on FK_id_grupo = id_grupo
+join docentes on id_docentes = FK_id_docente 
+inner join acudiente on id_acudiente = id_ninos;
+
+/* ==== 12. Mostrar asistencia completa niño, acudiente, docente, estado, fecha y grupo==== */
+
+select nombre_nino as Niño, nombre_grupo as Grupo, nombre_acudiente as Acudiente, nombre_docente as Docente, fecha_asistencia as Fecha, estado
+from ninos
+inner join grupos on FK_id_grupo = id_grupo
+join acudiente on id_ninos = id_acudiente
+join docentes on id_docentes = FK_id_docente
+join asistencia on id_ninos = FK_id_ninos
+join estadoasistencia on id_estado_asistencia = FK_id_estado_asistencia;
+
+/* ==== 13. Docente, grupo y actividad ====*/
+select nombre_docente as Docente, nombre_grupo as Grupo, descripcion_actividad as Actividad
+from docentes
+inner join grupos on FK_id_docente = id_docentes
+join grupoyactividad GA on GA.FK_id_grupo = id_grupo
+join actividades on id_actividades = FK_id_actividades;
+
+/* ==== 14. Niño, tipo de documento, documento y acudiente ====*/
+
+select nombre_nino AS nombre, apellido_nino AS apellido, documento_nino, tipo_documento AS tipodocumento, nombre_acudiente as Acudiente
+from ninos N
+inner join tipodocumento on N.FK_id_tipo_documento = id_tipo_documento
+inner join acudiente on id_acudiente = id_ninos;
